189f5786583826da161876f404941f56c699d188
Exequiel Raineri <exeraineri15@gmail.com>
2024-06-09 22:51:42 -0300

    delete gh -pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@
 import { Menu } from "./components/Menu";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
-import { Hero } from "./components/Hero";
 import { AboutMe } from "./components/AboutMe";
 import { Experience } from "./components/Experience";
 import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
 import { Footer } from "./components/Footer";
 import { Link } from "react-scroll";
-import video1 from "./assets/video-fondo1.mp4";
-import video2 from "./assets/video-fondo2.mp4";
 import { HeroV2 } from "./components/HeroV2/HeroV2";
 
 function App() {
